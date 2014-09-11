027301d219bf5d4f3e8a28d85d565fc16b671b66
Charles Brandt <charles.brandt@gmail.com>
2014-09-11 15:06:01 -0400

    updating geopy to the latest version from github

--- geopy/__init__.py
+++ geopy/__init__.py
@@ -1,13 +1,14 @@
+"""
+geopy is a Python 2 and 3 client for several popular geocoding web services.
+
+geopy makes it easy for Python developers to locate the coordinates of
+addresses, cities, countries, and landmarks across the globe using third-party
+geocoders and other data sources.
+"""
+
 from geopy.point import Point
 from geopy.location import Location
-from geopy import geocoders
+from geopy.geocoders import * # pylint: disable=W0401
 
-VERSION = (0, 95, 1)
 
-def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:]:
-        version = '%s.%s' % (version, VERSION[3])
-    return version
+__version__ = "1.1.5"
--- geopy/geocoders/geonames.py
+++ geopy/geocoders/geonames.py
@@ -1,63 +1,119 @@
-import xml.dom.minidom
-from urllib import urlencode
-from urllib2 import urlopen
-from geopy import util
+"""
+:class:`GeoNames` geocoder.
+"""
 
-try:
-    import json
-except ImportError:
-    try:
-        import simplejson as json
-    except ImportError:
-        from django.utils import simplejson as json
+from geopy.compat import urlencode
 
-from geopy.geocoders.base import Geocoder
+from geopy.geocoders.base import Geocoder, DEFAULT_TIMEOUT
+from geopy.location import Location
+from geopy.exc import (
+    GeocoderInsufficientPrivileges,
+    GeocoderServiceError,
+    ConfigurationError
+)
+from geopy.util import logger
 
-class GeoNames(Geocoder):
-    def __init__(self, format_string=None, output_format=None, country_bias=None):
-        if format_string != None:
-            from warnings import warn
-            warn('geopy.geocoders.geonames.GeoNames: The `format_string` parameter is deprecated.'+
-                ' (It has always been ignored for GeoNames.)', DeprecationWarning)
-        if output_format != None:
-            from warnings import warn
-            warn('geopy.geocoders.geonames.GeoNames: The `output_format` parameter is deprecated '+
-                 'and now ignored.', DeprecationWarning)
-        
+
+__all__ = ("GeoNames", )
+
+
+class GeoNames(Geocoder): # pylint: disable=W0223
+    """
+    GeoNames geocoder, documentation at:
+        http://www.geonames.org/export/geonames-search.html
+
+    Reverse geocoding also available, but not yet implemented. Documentation
+    at:
+        http://www.geonames.org/maps/us-reverse-geocoder.html
+    """
+
+    def __init__(
+            self,
+            country_bias=None,
+            username=None,
+            timeout=DEFAULT_TIMEOUT,
+            proxies=None
+        ):
+        """
+        :param string country_bias:
+
+        :param string username:
+
+        :param int timeout: Time, in seconds, to wait for the geocoding service
+            to respond before raising a :class:`geopy.exc.GeocoderTimedOut`
+            exception.
+
+            .. versionadded:: 0.97
+
+        :param dict proxies: If specified, routes this geocoder's requests
+            through the specified proxy. E.g., {"https": "192.0.2.0"}. For
+            more information, see documentation on
+            :class:`urllib2.ProxyHandler`.
+
+            .. versionadded:: 0.96
+        """
+        super(GeoNames, self).__init__(
+            scheme='http', timeout=timeout, proxies=proxies
+        )
+        if username == None:
+            raise ConfigurationError(
+                'No username given, required for api access.  If you do not '
+                'have a GeoNames username, sign up here: '
+                'http://www.geonames.org/login'
+            )
+        self.username = username
         self.country_bias = country_bias
-        self.url = "http://ws.geonames.org/searchJSON?%s"
-    
-    def geocode(self, string, exactly_one=True):
-        if isinstance(string, unicode):
-            string = string.encode('utf-8')
+        self.api = "%s://api.geonames.org/searchJSON" % self.scheme
+
+    def geocode(self, query, exactly_one=True, timeout=None): # pylint: disable=W0221
+        """
+        Geocode a location query.
+
+        :param string query: The address or query you wish to geocode.
+
+        :param bool exactly_one: Return one result or a list of results, if
+            available.
+
+        :param int timeout: Time, in seconds, to wait for the geocoding service
+            to respond before raising a :class:`geopy.exc.GeocoderTimedOut`
+            exception. Set this only if you wish to override, on this call
+            only, the value set during the geocoder's initialization.
+
+            .. versionadded:: 0.97
+        """
         params = {
-            'q': string
+            'q': query,
+            'username': self.username
         }
         if self.country_bias:
             params['countryBias'] = self.country_bias
-        
-        url = self.url % urlencode(params)
-        return self.geocode_url(url, exactly_one)
-    
-    def geocode_url(self, url, exactly_one=True):
-        page = urlopen(url)
-        return self.parse_json(page, exactly_one)
-    
-    def parse_json(self, page, exactly_one):
-        if not isinstance(page, basestring):
-            page = util.decode_page(page)
-            
-        doc = json.loads(page)
+        if exactly_one is True:
+            params['maxRows'] = 1
+        url = "?".join((self.api, urlencode(params)))
+        logger.debug("%s.geocode: %s", self.__class__.__name__, url)
+        return self._parse_json(
+            self._call_geocoder(url, timeout=timeout),
+            exactly_one
+        )
+
+    def _parse_json(self, doc, exactly_one):
+        """
+        Parse JSON response body.
+        """
         places = doc.get('geonames', [])
-        
-        if not places:
+        err = doc.get('status', None)
+        if err and 'message' in err:
+            if err['message'].startswith("user account not enabled to use"):
+                raise GeocoderInsufficientPrivileges(err['message'])
+            else:
+                raise GeocoderServiceError(err['message'])
+        if not len(places):
             return None
-        
-        if exactly_one and len(places) != 1:
-            raise ValueError("Didn't find exactly one code! " \
-                             "(Found %d.)" % len(places))
-        
+
         def parse_code(place):
+            """
+            Parse each record.
+            """
             latitude = place.get('lat', None)
             longitude = place.get('lng', None)
             if latitude and longitude:
@@ -65,17 +121,17 @@ class GeoNames(Geocoder):
                 longitude = float(longitude)
             else:
                 return None
-            
+
             placename = place.get('name')
             state = place.get('adminCode1', None)
             country = place.get('countryCode', None)
-            
-            location = ', '.join(filter(lambda x: bool(x),
-                [placename, state, country]
-            ))
-            
-            return (location, (latitude, longitude))
-        
+
+            location = ', '.join(
+                [x for x in [placename, state, country] if x]
+            )
+
+            return Location(location, (latitude, longitude), place)
+
         if exactly_one:
             return parse_code(places[0])
         else:
